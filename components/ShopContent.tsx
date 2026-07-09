f07937bd2ea4616cf4cb16a71e9223db45f387d8
rishabhpatel0 <rishabhpatel00042@gmail.com>
2026-07-09 20:32:36 +0530

    Sync shop filters with URL parameters

--- components/ShopContent.tsx
+++ components/ShopContent.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import { useEffect } from "react";
+import {
+  useSearchParams,
+  useRouter,
+  usePathname,
+} from "next/navigation";
+
+import { useShop } from "@/context/ShopContext";
 import Header from "./layout/Header";
 import Sidebar from "./layout/Sidebar";
 import ProductListing from "./ProductListing";
@@ -10,6 +18,60 @@ type Props = {
 };
 
 export default function ShopContent({ products }: Props) {
+    
+    const {
+        search,
+        setSearch,
+        selectedCategory,
+        setSelectedCategory,
+        maxPrice,
+        setMaxPrice,
+      } = useShop();
+      
+      const searchParams = useSearchParams();
+      const router = useRouter();
+      const pathname = usePathname();
+
+      useEffect(() => {
+        const urlSearch = searchParams.get("search");
+        const urlCategory = searchParams.get("category");
+        const urlPrice = searchParams.get("price");
+      
+        if (urlSearch) {
+          setSearch(urlSearch);
+        }
+      
+        if (urlCategory) {
+          setSelectedCategory(urlCategory);
+        }
+      
+        if (urlPrice) {
+          setMaxPrice(Number(urlPrice));
+        }
+      }, []);
+
+      useEffect(() => {
+        const params = new URLSearchParams();
+      
+        if (search) {
+          params.set("search", search);
+        }
+      
+        if (selectedCategory !== "all") {
+          params.set("category", selectedCategory);
+        }
+      
+        params.set("price", String(maxPrice));
+      
+        router.replace(`${pathname}?${params.toString()}`);
+      }, [
+        search,
+        selectedCategory,
+        maxPrice,
+        router,
+        pathname,
+      ]);
+      
   return (
     <>
       <Header />
